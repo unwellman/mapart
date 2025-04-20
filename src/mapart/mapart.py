fc311f788174b6aebfde802c4b504ea849b324e7
unwellman <noah77w@gmail.com>
2025-04-20 05:54:21 -0700

    Palette loading tests passing

--- src/mapart/mapart.py
+++ src/mapart/mapart.py
@@ -21,13 +21,21 @@ class Palette:
     """ Contain Minecraft block palette info including block names, slopes, and
     their respective colors
     """
-    def __init__ (self, fp):
-        """ fp: a CSV containing block color data (courtesy of minecraft.wiki)
+    def __init__ (self, fp, mode="tool"):
+        """ Initialize the object and load the color palette
+        Parameters:
+            fp: a CSV containing block color data (courtesy of minecraft.wiki)
+            mode: a string matching "stairs", "flat", or "tool", affecting how
+            many shades can be accessed
         """
+        assert mode in ["stairs", "flat", "tool"]
         frm = pd.read_csv(fp, sep=',', encoding='utf8')
         assert tuple(frm.keys()) == ("ID", "Color", "RGB", "Blocks")
+
         frm = frm.drop(0) # Delete transparent line
+
         self.frame = self.__format_frame(frm)
+        self.ids, self.colors = self.__init_palette(mode)
 
     def __format_frame (self, frame):
         frame.pop("Color") # Empty column
@@ -39,6 +47,40 @@ class Palette:
         frame.insert(2, "Blocks", block_lists)
 
         return frame
+
+    def __init_palette (self, mode):
+        """ Return Minecraft color ID's and RGB color palette
+
+        Note that +1 is added to the ID's to accomodate the 4 transparent colors
+        """
+        # Weird hack to keep types in order
+        base = np.array([list(row) for row in self.frame["RGB"]])
+        r, c = base.shape
+
+        if mode == "flat":
+            ids = 4*np.arange(r) + 1
+            return ids + 4, np.array(220.0/255.0*base)
+
+        elif mode == "stairs":
+            colors = np.empty((3*r, c))
+            colors[0::3] = np.array(180.0/255.0*base)
+            colors[1::3] = np.array(220.0/255.0*base)
+            colors[2::3] = np.array(255.0/255.0*base)
+            ids = np.empty(3*r, dtype="int64")
+            ids[0::3] = 4*np.arange(r) + 0
+            ids[1::3] = 4*np.arange(r) + 1
+            ids[2::3] = 4*np.arange(r) + 2
+            return ids + 4, colors
+
+        else: # mode == "tool"
+            num_shades = 4
+            ids = np.arange(r*4, dtype="int64")
+            colors = np.empty((4*r, c))
+            colors[0::4] = np.array(180.0/255.0*base)
+            colors[1::4] = np.array(220.0/255.0*base)
+            colors[2::4] = np.array(255.0/255.0*base)
+            colors[3::4] = np.array(135.0/255.0*base)
+            return ids + 4, colors
 
     @staticmethod
     def __split_blocks (blocks, sep=', '):
--- tests/test_mapart.py
+++ tests/test_mapart.py
@@ -1,16 +1,36 @@
 from mapart import Mapart, Palette
 import numpy as np
 
+COLOR_TABLE_FP = "res/map_colors_1.21.csv"
+
 def test_palette_blocks ():
-    palette = Palette("res/map_colors_1.21.csv")
+    palette = Palette(COLOR_TABLE_FP)
 
     # Test case to check proper string splitting, verified for 1.21 color data
     test_str = "birch (planks, log (vertical), stripped log, wood, stripped wood, sign, pressure plate, trapdoor, stairs, slab, fence gate, fence, door)"
     assert palette.frame["Blocks"][2][2] == test_str
 
 def test_palette_rgb ():
-    palette = Palette("res/map_colors_1.21.csv")
+    palette = Palette(COLOR_TABLE_FP)
     assert np.allclose(palette.frame["RGB"][1], [127, 178, 56])
+
+def test_palette_shades_stairs ():
+    palette = Palette(COLOR_TABLE_FP, mode="stairs")
+    assert palette.colors.shape == (61*3, 3)
+    assert np.allclose(palette.ids[0:4], np.array([4, 5, 6, 8]))
+    assert np.allclose(palette.colors[1], 220/255*np.array([127, 178, 56]))
+
+def test_palette_shades_flat ():
+    palette = Palette(COLOR_TABLE_FP, mode="flat")
+    assert palette.colors.shape == (61, 3)
+    assert palette.ids[3] == 17
+    assert np.allclose(palette.colors[1], 220/255*np.array([247, 233, 163]))
+
+def test_palette_shades_tool ():
+    palette = Palette(COLOR_TABLE_FP, mode="tool")
+    assert palette.colors.shape == (61*4, 3)
+    assert np.allclose(palette.ids[0:4], np.array([4, 5, 6, 7]))
+    assert np.allclose(palette.colors[1], 220/255*np.array([127, 178, 56]))
 
 
 
