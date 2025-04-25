luma/chroma indexing error fix, luma weighting tests

src/mapart/__main__.py
@@ -1,7 +1,31 @@
-import mapart
+from mapart import *
+from PIL import Image as img
+import sys
+import os
+
+COLOR_TABLE_FP = "res/map_colors_1.21.csv"
 
 def main():
-    pass
+    # Basic, basic single argument usage
+    fp = sys.argv[1]
+    assert os.path.isfile(fp)
+    name = os.path.splitext(os.path.basename(fp))[0]
+    palette = Palette(COLOR_TABLE_FP, mode="stairs")
+    weights = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    luma_weights = [0.1, 0.5, 1.0, 2.0, 10.0]
+    for w in weights:
+        for l in luma_weights:
+            art = Mapart(fp, scheme="dither", dither_weight=w)
+            art.rgb = chroma(art.rgb)
+            art.rgb[:, :, 0] *= l
+            img.fromarray(art.rgb.astype(np.uint8)).save("tmp/wtf.jpg")
+            colors = chroma(palette.colors)
+            colors[:, 0] *= l
+            _, idx = art.generate(colors)
+            im = palette.get_image_index(idx)
+            out_fp = f"tmp/{name}_fs{w}_lc{l}.jpg"
+            im.save(out_fp)
+            print(f"Wrote {out_fp}")
 
 if __name__ == "__main__":
     main()

src/mapart/img_proc.py
@@ -2,7 +2,7 @@ import numpy as np
 import PIL
 
 # Courtesy of Wikipedia: convert rgb to brightness/hue/saturation
-perception_matrix = np.array([[0.2126, 0.7512, 0.0722],
+perception_matrix = np.array([[0.299, 0.587, 0.114],
                               [1.0, -0.5, -0.5],
                               [0.0, np.sqrt(3)/2, -np.sqrt(3)/2]])
 perception_matrix_inv = np.linalg.inv(perception_matrix)
@@ -32,9 +32,9 @@ def chroma (rgb, axis=-1):
         return perception_matrix @ rgb
     shp = rgb.shape
     assert shp[-1] == 3
-    ret = np.reshape(rgb, (3, np.prod(shp[0:-1])))
-    ret = perception_matrix.T @ ret
-    return np.reshape(ret, shp)
+    ret = np.reshape(rgb, (np.prod(shp[0:-1]), 3))
+    ret = perception_matrix @ ret.T
+    return np.reshape(ret.T, shp)
 
 def rgb (chroma):
     """Compute rgb from luma and chroma values
@@ -44,6 +44,6 @@ def rgb (chroma):
         return perception_matrix_inv @ rgb
     shp = rgb.shape
     assert shp[-1] == 3
-    ret = np.reshape(rgb, (3, np.prod(shp[0:-1])))
-    ret = perception_matrix_inv.T @ ret
-    return np.reshape(ret, shp)
+    ret = np.reshape(rgb, (np.prod(shp[0:-1]), 3))
+    ret = perception_matrix_inv @ ret.T
+    return np.reshape(ret.T, shp)

src/mapart/mapart.py
@@ -9,7 +9,7 @@ class Mapart:
     """ Methods specific to Minecraft map art
     """
     
-    def __init__ (self, fp, scheme="dither", dither_weight=1.0):
+    def __init__ (self, fp, scheme="dither", dither_weight=1.0, luma_weight=None):
         """ Initializer
         Load image with PIL and store with numpy array
         """
@@ -18,7 +18,6 @@ class Mapart:
         self.dither_weight = dither_weight
         with img.open(fp) as im:
             self.rgb = np.array(im, dtype="float64")
-        self.lc = img_proc.chroma(self.rgb)
 
     def generate (self, palette):
         """ Generate a map art based on the state of the object
@@ -75,6 +74,16 @@ class Palette:
 
         self.frame = self.__format_frame(frm)
         self.ids, self.colors = self.__init_palette(mode)
+
+    def get_image_index (self, idx):
+        """ Turn an np.array of indices into a PIL image
+        """
+        assert idx.ndim == 2
+        ret = np.empty((*(idx.shape), 3), dtype="float64")
+        for r in range(idx.shape[0]):
+            for c in range(idx.shape[1]):
+                ret[r, c, :] = self.colors[idx[r, c]]
+        return img.fromarray(ret.astype(np.uint8))
 
     def __format_frame (self, frame):
         frame.pop("Color") # Empty column

tests/test_img_proc.py
@@ -3,12 +3,13 @@ from PIL import Image as img
 from mapart import img_proc
 
 def test_chroma ():
-    test = np.array([[255.0, 0, 0], [0, 255, 0], [0, 0, 255]])
-    exp = np.array([54.213, 255, 0])
+    test = np.array([[255.0, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 255.0]])
+    exp = np.array([76.245, 255, 0])
     assert np.allclose(img_proc.chroma(test)[0], exp)
+    assert np.allclose(img_proc.chroma(test)[3], [255.0, 0, 0])
 
 def test_rgb ():
-    test = np.array([54.213, 255.0, 0.0])
+    test = np.array([76.245, 255.0, 0.0])
     exp = np.array([255.0, 0, 0])
     assert np.allclose(img_proc.rgb(test), exp)
 